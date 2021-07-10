9a076aead17dfee324654f15ecfe27f476c8e713
0dr4d3k <odradek@wanadoo.es>
2021-07-10 19:55:40 +0200

    [all] Playground con custom generator. [build]  Buid funcionando para blocks y core. [next]  Generar npm run build:generator-arduino

---
+++ generators/custom_generator.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+codelabGenerator = new Blockly.Generator('JSON');
+
+sampleGenerator['object'] = function(block) {
+    return 'my code string';
+  }
+sampleGenerator['member'] = function(block) {
+    return 'my member';
+  }
